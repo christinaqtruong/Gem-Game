b12aad215850061b1d0c01ad596453c25c8c960f
Christina Truong <christinaqtruong@gmail.com>
2019-06-03 22:04:25 -0500

    added win/loss conditions to each button click by assigning it to a variable called winner

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -45,64 +45,85 @@ var letsPlay = $("#start").on("click", function(){
     var pinksValue = (Math.floor(Math.random() * 12));
     console.log(pinksValue);
 
-    /*If the player's total does not equal the random generator number, then clicking Garnet's button adds 
-    her value to the total score and displays the new total on the screen*/
-    if (total != number){
-        garnet.on("click", function(){
-            total = total + garnetsValue;
-            playerScore.text(total);
-        })  
-        
-        amethyst.on("click", function(){
-            total = total + amethystsValue;
-            console.log(total);
-            playerScore.text(total);  
-        })
+  //If the player's total score equals the random number generated, display winning statements, add 1 the win counter and reveal the new button displaying the text "play again"
+  var winner = function(){
     
-        pearl.on("click", function(){
-            total = total + pearlsValue;
-            console.log(total);
-            playerScore.text(total);  
-        })
-   
-        pink.on("click", function(){
-            total = total + pinksValue;
-            console.log(total);
-            playerScore.text(total);   
-        })
-        }
-    
-    //If the player's total score equals the random number generated, display winning statements, add 1 the win counter and reveal the new button displaying the text "play again"
     if (total === number){
                 
             console.log("You win!")
         
             wins++
             console.log(wins)
+
             $(".wins").text("Number of wins: " + wins)
             $("#start").show();
             $("#start").text("Play again")
             $(".winner").text("Congrats! You won!")
 
             lockGame = false;
-        }
+
+            garnet.text(garnetsValue);
+            pearl.text(pearlsValue);
+            amethyst.text(amethystsValue);
+            pink.text(pinksValue);
+        } else
     
     
 //     //    * The player loses if their score goes above the random number.
-//     if (total > number){
+    if (total > number){
             
-//     console.log("You lost!")
-    
-//     losses++
-//     console.log(losses)
-//     $(".wins").text("Number of losses: " + losses)
-    
-//         $(".loser").text("Oops! You busted! Click Start Over to play again.")
-//         $("#start").text("Start Over")
+        console.log("You lost!")
         
-//         lockGame = false;
-//     }
+        losses++
+        console.log(losses)
+        
+        $(".losses").text("Number of losses: " + losses)
+        $("#start").show();
+        $(".loser").text("Oops! You busted! Click Start Over to play again.")
+        $("#start").text("Start Over")
+        
+        lockGame = false;
 
+        garnet.text(garnetsValue);
+        pearl.text(pearlsValue);
+        amethyst.text(amethystsValue);
+        pink.text(pinksValue);
+    }
+}
+
+
+    /*If the player's total does not equal the random generator number, then clicking Garnet's button adds 
+    her value to the total score and displays the new total on the screen*/
+    if (total != number){
+        garnet.on("click", function(){
+            total = total + garnetsValue;
+            playerScore.text(total);
+            winner();
+        })  
+        
+        amethyst.on("click", function(){
+            total = total + amethystsValue;
+            console.log(total);
+            playerScore.text(total);
+            winner();  
+        })
+    
+        pearl.on("click", function(){
+            total = total + pearlsValue;
+            console.log(total);
+            playerScore.text(total);  
+            winner();
+        })
+   
+        pink.on("click", function(){
+            total = total + pinksValue;
+            console.log(total);
+            playerScore.text(total);
+            winner();   
+        })
+        }
+    
+  
 })
 })
 
